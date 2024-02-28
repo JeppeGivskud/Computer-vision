cb399fb844dcc10e0a77a2a4e4ad0ef5279f13f2
Christoffer <clyhol20@student.aau.dk>
2024-02-28 10:56:06 +0100

    aaaaaaaa

--- K3/Exercise_3/Warmup.py
+++ K3/Exercise_3/Warmup.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 import numpy as np
 
-input_image = cv.imread("Computer-vision\K3\Exercise_3\aau-city-1.jpg")
-output_image = cv.imread("Computer-vision\K3\Exercise_3\aau-city-1.jpg")
+input_image = cv.imread("Exercises\\Exercise_3\\aau-city-1.jpg")
+output_image = input_image.copy()
 
 ############################################################
 # Use cv2.cornerHarris to find corners and draw them with circles on output picture
@@ -11,9 +11,17 @@ output_image = cv.imread("Computer-vision\K3\Exercise_3\aau-city-1.jpg")
 gray = cv.cvtColor(input_image, cv.COLOR_BGR2GRAY)
 gray = np.float32(gray)
 
-corner = cv.cornerHarris(gray, 2, 3, 0.04)
-corner = cv.dilate()
 
+corner = cv.cornerHarris(gray, 2, 3, 0.04)
+corner = cv.dilate(corner, None)
+
+threshold = 0.5 * corner.max()
+
+for y in range(corner.shape[0]):
+    for x in range(corner.shape[1]):
+        if corner[y, x] > threshold:
+            output_image = cv.circle(output_image, (x, y), 10, (255, 0, 0))
 ############################################################
 
-cv.imwrite("output.png", output_image)
+cv.imshow("output.png", output_image)
+cv.waitKey(0)
