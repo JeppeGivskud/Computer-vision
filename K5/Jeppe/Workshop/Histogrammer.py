06094d4b50240c6cb7277c77d02600e5e405241d
JeppeGivskud <jgivsk20@student.aau.dk>
2024-03-14 14:40:22 +0100

    Videoes

--- K5/Jeppe/Workshop/Histogrammer.py
+++ K5/Jeppe/Workshop/Histogrammer.py
@@ -2,13 +2,16 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-template = cv.imread("biker copy.png")
+template = cv.imread("Minion.png")
 hsv_template = cv.cvtColor(template, cv.COLOR_BGR2HSV)
 
 # make a mask of the template
 template_hist_hue = cv.calcHist([hsv_template], [0], None, [180], [0, 180])
 template_hist_satu = cv.calcHist([hsv_template], [1], None, [256], [0, 256])
-# cv.normalize(template_hist_satu, template_hist_satu, 0, 255, cv.NORM_MINMAX)
+
+# Change to normalized values (so amount of pixels doesn't matter)
+cv.normalize(template_hist_hue, template_hist_hue, 0, 255, cv.NORM_MINMAX)
+cv.normalize(template_hist_satu, template_hist_satu, 0, 255, cv.NORM_MINMAX)
 
 
 def plotme(name, xlabel, ylabel, data, limits):
@@ -21,11 +24,6 @@ def plotme(name, xlabel, ylabel, data, limits):
     plt.show()
 
 
-cv.imwrite("output.png", template)
-print(template_hist_hue)
-template_hist_hue = template_hist_hue * 2
-print(template_hist_hue)
-
-plotme("Histogram Hue", "Hue", "Frequency", template_hist_hue, [0, 360])
+plotme("Histogram Hue", "Hue", "Frequency", template_hist_hue, [0, 180])
 
 plotme("Histogram Saturation", "Saturation", "Frequency", template_hist_satu, [0, 255])
